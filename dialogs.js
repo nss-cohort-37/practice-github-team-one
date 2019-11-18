6a01bed887cc2898370cc6545d235a6a71d5013f
Steve Brownlee <chortlehoort@gmail.com>
2019-11-18 14:24:42 -0600

    All event listeners added

--- dialogs.js
+++ dialogs.js
@@ -25,6 +25,47 @@ const initializeDetailButtonEvents = () => {
         }
     )
 
+    document.querySelector("#button--jawsie").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--jawsie")
+            theDialog.showModal()
+        }
+    )
+
+
+    document.querySelector("#button--dangles").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--dangles")
+            theDialog.showModal()
+        }
+    )
+
+    document.querySelector("#button--sam").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--sam")
+            theDialog.showModal()
+        }
+    )
+
+    document.querySelector("#button--barry").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--barry")
+            theDialog.showModal()
+        }
+    )
+
+    document.querySelector("#button--snappy").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--snappy")
+            theDialog.showModal()
+        }
+    )
+
 }
 
 export default initializeDetailButtonEvents
